8aedb9ef7e7c8852b8d2d30a249ba7038dda5343
PakBras <mrbrassika@gmail.com>
2023-06-11 16:08:35 +0800

    sql connect

--- include/dbh-inc.php
+++ include/dbh-inc.php
@@ -3,3 +3,10 @@
 $dbServername = "localhost";
 $dbUsername = "root";
 $dbPassword = "";
+$dbName = "game" ;
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+if (!$conn) {
+    echo "connected succesfully";
+}
+?>
--- submit.php
+++ submit.php
@@ -1,3 +1,7 @@
+<?php
+    include_once'includes/dbh-inc.php';
+?>
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -38,6 +42,8 @@
     </nav>
   </header>
   <body>
+    $conn;
+    
     <h1><button type="button" class="btn btn-primary" href="">Submit here</button></h1>
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
